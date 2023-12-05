3fb9eaa76d2c9bae70944601a920b30ae6f658fa
jomunji <john04munns@gmail.com>
2023-12-05 12:57:56 +0000

    Made home page and linked it to the project

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+#This function renders the home.html file when called.
+def home(request): #The request argument is created whenever a page loads, it contains metadata
+    return render(request,"home/home.html", {})
