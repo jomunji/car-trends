cfb0a3b44a5214ecaea200367c982a385a09835a
jomunji <john04munns@gmail.com>
2023-12-12 17:07:02 +0000

    Got the database link working

--- home/models.py
+++ home/models.py
@@ -4,20 +4,18 @@ from django.db import models
 class CarDb(models.Model):
     maker = models.CharField(max_length=30, null=True)
     genmodel = models.CharField(max_length=30, null=True)
-    genmodel_id = models.CharField(max_length=30, null=True)
-    adv_id = models.CharField(max_length=30, null=True)
-    adv_year = models.CharField(max_length=30, null=True)
-    adv_month = models.CharField(max_length=30, null=True)
+    adv_year = models.IntegerField(null=True)
+    adv_month = models.IntegerField(null=True)
     colour = models.CharField(max_length=30, null=True)
-    reg_year = models.CharField(max_length=30, null=True)
+    reg_year = models.IntegerField(null=True)
     bodytype = models.CharField(max_length=30, null=True)
-    mileage = models.CharField(max_length=30, null=True)
-    engine_size = models.CharField(max_length=30, null=True)
+    mileage = models.IntegerField(null=True)
+    engine_size = models.FloatField(null=True)
     gearbox = models.CharField(max_length=30, null=True)
     fuel_type = models.CharField(max_length=30, null=True)
-    price = models.CharField(max_length=30, null=True)
-    seat_num = models.CharField(max_length=30, null=True)
-    door_num = models.CharField(max_length=30, null=True)
+    price = models.IntegerField(null=True)
+    seat_num = models.IntegerField(null=True)
+    door_num = models.IntegerField(null=True)
 
     class Meta:
         db_table = "ad_table"
--- home/views.py
+++ home/views.py
@@ -1,34 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .models import *
 # Create your views here.
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 #This function renders the home.html file when called.
 def home(request): #The request argument is created whenever a page loads, it contains metadata
-    messages.info(request,"Welcome to home page")
-    return render(request,"home/home.html", {})
 
-from .models import *
-def index(request):
     if request.method == 'POST':
-        if request.POST.get('name'):
-            table = CarDb()
-            table.maker = request.POST.get('maker')
-            table.genmodel = request.POST.get('genmodel')
-            table.genmodel_id = request.POST.get('genmodel_id')
-            table.adv_id = request.POST.get('adv_id')
-            table.adv_year = request.POST.get('adv_year')
-            table.adv_month = request.POST.get('adv_month')
-            table.colour = request.POST.get('colour')
-            table.reg_year = request.POST.get('reg_year')
-            table.bodytype = request.POST.get('bodytype')
-            table.mileage = request.POST.get('mileage')
-            table.engine_size = request.POST.get('engine_size')
-            table.gearbox = request.POST.get('gearbox')
-            table.fuel_type = request.POST.get('fuel_type')
-            table.price = request.POST.get('price')
-            table.seat_num = request.POST.get('seat_num')
-            table.door_num = request.POST.get('door_num')
-            table.save()
-            messages.success(request,"Record saved successfully")
-        return redirect('carlist')
-    return render(request,'home/home.html')
+        if request.POST.get('maker'):
+            try:
+                table = CarDb()
+                table.maker = request.POST.get('maker')
+                table.genmodel = request.POST.get('genmodel')
+                table.adv_year = request.POST.get('adv_year')
+                table.adv_month = request.POST.get('adv_month')
+                table.colour = request.POST.get('colour')
+                table.reg_year = request.POST.get('reg_year')
+                table.bodytype = request.POST.get('bodytype')
+                table.mileage = request.POST.get('mileage')
+                table.engine_size = request.POST.get('engine_size')
+                table.gearbox = request.POST.get('gearbox')
+                table.fuel_type = request.POST.get('fuel_type')
+                table.price = request.POST.get('price')
+                table.seat_num = request.POST.get('seat_num')
+                table.door_num = request.POST.get('door_num')
+                table.save()
+                messages.success(request,"Record saved successfully")
+            except:
+                logger.warning("Data posting failed")
+                messages.error(request,"Data posting failed!")
+        #return redirect('carlist')
+    return render(request,'home/home.html',{})
+
