ddedc9ed261d98b0c8201e14c61e28960bb873d1
jomunji <john04munns@gmail.com>
2023-12-13 01:16:38 +0000

    Added data and first visual

--- home/views.py
+++ home/views.py
@@ -1,38 +1,60 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.db import connection
+from django.http import HttpResponse
 from .models import *
-# Create your views here.
 
 import logging
-
 logger = logging.getLogger(__name__)
 
 #This function renders the home.html file when called.
 def home(request): #The request argument is created whenever a page loads, it contains metadata
 
-    if request.method == 'POST':
-        if request.POST.get('maker'):
-            try:
-                table = CarDb()
-                table.maker = request.POST.get('maker')
-                table.genmodel = request.POST.get('genmodel')
-                table.adv_year = request.POST.get('adv_year')
-                table.adv_month = request.POST.get('adv_month')
-                table.colour = request.POST.get('colour')
-                table.reg_year = request.POST.get('reg_year')
-                table.bodytype = request.POST.get('bodytype')
-                table.mileage = request.POST.get('mileage')
-                table.engine_size = request.POST.get('engine_size')
-                table.gearbox = request.POST.get('gearbox')
-                table.fuel_type = request.POST.get('fuel_type')
-                table.price = request.POST.get('price')
-                table.seat_num = request.POST.get('seat_num')
-                table.door_num = request.POST.get('door_num')
-                table.save()
-                messages.success(request,"Record saved successfully")
-            except:
-                logger.warning("Data posting failed")
-                messages.error(request,"Data posting failed!")
-        #return redirect('carlist')
     return render(request,'home/home.html',{})
 
+
+
+import pandas as pd
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvas
+
+#This function returns a list of tuples of RGB values.
+#I used it to colour all of my plots.
+def randomRGB(length):
+    colourList = []
+    if length == 1:  #If length is 1 return a single tuple.
+        return (random.uniform(0,1),random.uniform(0,1),random.uniform(0,1))
+    else:  #If length is greater than 1:
+        for i in range(length): #Loop for given range
+            rgbtup = (random.uniform(0,1),random.uniform(0,1),random.uniform(0,1))#For each iteration create a tuple...
+            colourList.append(rgbtup) #...and add it to the list
+        return(colourList) #return the final list
+
+def plots(request): #The request argument is created whenever a page loads, it contains metadata
+    
+    #Fetch all data from database
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id,maker FROM ad_table")
+        columns = [col[0] for col in cursor.description]
+        data = cursor.fetchall()
+    df = pd.DataFrame(data, columns=columns)
+    # logger.warning(df.head())
+
+    #Create home visual
+    matplotlib.use("agg")
+    fig, ax = plt.subplots()
+    home_visual_df = df.groupby("maker", as_index=False).count().sort_values(by="id", ascending=False)[:20]
+    # logger.warning(home_visual_df.sort_values(by="id"))
+    # logger.warning("---------")
+    # logger.warning(home_visual_df.columns)
+    ax.bar(home_visual_df["maker"], home_visual_df["id"], color=randomRGB(20))
+    ax.set_title("Manufacturers")
+    plt.xticks(rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+
+    canvas = FigureCanvas(fig)
+    response = HttpResponse(content_type='image/png')
+    canvas.print_png(response)
+    return response
